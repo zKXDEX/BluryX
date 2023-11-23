e81010d3b1ba647dd21bed5e35fb382a894d5521
zKXDEX <deiividdlk@gmail.com>
2023-11-23 04:39:45 +0100

    Update Readme

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function EnableBluryx(window: BufferSource, enable: boolean): any;
-export declare function ExtendFrameClientArea(window: BufferSource, left: number, top: number, right: number, bottom: number): any;
